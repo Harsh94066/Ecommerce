cb835ce68a08c8ad7e1cd48a3a0edcc19286201e
Harsh-Chopra <harshchopra9406626920@gmail.com>
2022-02-06 17:35:27 +0530

    Server changes

--- server.js
+++ server.js
@@ -36,6 +36,16 @@ readdirSync('./routes').map((r) =>
     app.use('/api', require('./routes/' + r))
 );
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 // const router = require('..')
 // app.use('/api',router)
 
